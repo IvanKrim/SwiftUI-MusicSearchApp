ac47ee04559a38994e8adf300caf05ae215a6117
Ivan Kalabishka <reamonn96@gmail.com>
2021-07-08 22:35:20 +0300

    Initial Commit

---
+++ SwiftUI-MusicSearchApp/SwiftUI_MusicSearchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MusicSearchAppApp.swift
+//  SwiftUI-MusicSearchApp
+//
+//  Created by Kalabishka Ivan on 08.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MusicSearchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
